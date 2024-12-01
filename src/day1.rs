faa32ee40ba403e595198e321dc3c738133c0cba
Lydxn <ho3.14159@gmail.com>
2024-12-01 01:18:48 -0800

    a

---
+++ src/day1.rs
@@ -0,0 +1,11 @@
+#[aoc(day1, part1)]
+pub fn part1(s: &str) -> i32 {
+    let input = s;
+    input.len() as i32
+}
+
+#[aoc(day1, part2)]
+pub fn part2(s: &str) -> i32 {
+    let input = s;
+    input.len() as i32
+}
