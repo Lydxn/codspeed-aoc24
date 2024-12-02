b1f7672c87ab6df6f45254b14391f080e4765431
Lydxn <ho3.14159@gmail.com>
2024-12-01 23:18:26 -0800

    final

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,12 @@
 const N: usize = 1000;
 
-fn radix_sort_u17(arr: &mut [u32; N]) {
-    let mut cnt_lo = [0; 256];
-    let mut cnt_hi = [0; 512];
+unsafe fn radix_sort_u17(arr: &mut [u32; N]) {
+    let mut cnt_lo: [u16; 256] = [0; 256];
+    let mut cnt_hi: [u16; 512] = [0; 512];
 
-    for &x in arr.iter() {
-        cnt_lo[(x & 255) as usize] += 1;
-        unsafe {
-            *cnt_hi.get_unchecked_mut((x >> 8) as usize) += 1;
-        }
+    for x in arr.iter() {
+        *cnt_lo.get_unchecked_mut((*x & 255) as usize) += 1;
+        *cnt_hi.get_unchecked_mut((*x >> 8) as usize) += 1;
     }
 
     for i in 0..255 { cnt_lo[i + 1] += cnt_lo[i]; }
@@ -16,20 +14,16 @@ fn radix_sort_u17(arr: &mut [u32; N]) {
 
     let mut buf = [0; N];
 
-    for &x in arr.iter().rev() {
-        unsafe {
-            let ptr = cnt_lo.get_unchecked_mut((x & 255) as usize);
-            *ptr -= 1;
-            *buf.get_unchecked_mut(*ptr) = x;
-        }
+    for x in arr.iter().rev() {
+        let ptr = cnt_lo.get_unchecked_mut((*x & 255) as usize);
+        *ptr -= 1;
+        *buf.get_unchecked_mut(*ptr as usize) = *x;
     }
 
-    for &x in buf.iter().rev() {
-        unsafe {
-            let ptr = cnt_hi.get_unchecked_mut((x >> 8) as usize);
-            *ptr -= 1;
-            *arr.get_unchecked_mut(*ptr) = x;
-        }
+    for x in buf.iter().rev() {
+        let ptr = cnt_hi.get_unchecked_mut((*x >> 8) as usize);
+        *ptr -= 1;
+        *arr.get_unchecked_mut(*ptr as usize) = *x;
     }
 }
 
@@ -54,23 +48,19 @@ pub fn part1(input: &str) -> u32 {
         right[i] = parse_5b(&s[i*14+8..]);
     }
 
-    radix_sort_u17(&mut left);
-    radix_sort_u17(&mut right);
+    unsafe {
+        radix_sort_u17(&mut left);
+        radix_sort_u17(&mut right);
+    }
 
     left.iter().zip(&right)
         .map(|(a, &b)| a.abs_diff(b))
         .sum()
 }
 
-#[derive(Clone, Copy)]
-struct HashEntry {
-    key: u32,
-    value: u16,
-}
-
 #[aoc(day1, part2)]
 pub fn part2(input: &str) -> u32 {
-    let mut assoc = [HashEntry { key: 0, value: 0 }; 2048];
+    let mut assoc = [0; 2048];
 
     let s = input.as_bytes();
     for i in 0..N {
@@ -78,12 +68,12 @@ pub fn part2(input: &str) -> u32 {
         let mut h = right & 2047;
         loop {
             let entry = &mut assoc[h as usize];
-            if entry.key == 0 {
-                *entry = HashEntry { key: right, value: 1 };
+            if *entry == 0 {
+                *entry = right | 1 << 20;
                 break;
             }
-            if entry.key == right {
-                entry.value += 1;
+            if (*entry & 0xfffff) == right {
+                *entry += 1 << 20;
                 break;
             }
             h = (h + 1) & 2047;
@@ -97,11 +87,11 @@ pub fn part2(input: &str) -> u32 {
         let mut h = left & 2047;
         loop {
             let entry = assoc[h as usize];
-            if entry.key == 0 {
+            if entry == 0 {
                 break;
             }
-            if entry.key == left {
-                answer += left * (entry.value as u32);
+            if (entry & 0xfffff) == left {
+                answer += left * (entry >> 20);
             }
             h = (h + 1) & 2047;
         }
